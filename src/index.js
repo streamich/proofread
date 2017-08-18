49f32f535fce27aff3569990d91cfd9fbe9dbac1
streamich <team@streamich.com>
2017-08-18 11:55:58 +0100

    Reader object

--- src/index.js
+++ src/index.js
@@ -49,3 +49,73 @@ export const read = (iterator1, iterator2) => {
         }
     }
 };
+
+
+function getIteratorOrThrow(iter) {
+    if(typeof iter === 'function') iter = iter();
+
+    if(!iter || (typeof iter !== 'object') || (typeof iter.next !== 'function'))
+        throw new TypeError('Expected saga to be an iterator or a generator function.');
+
+    return iter;
+}
+
+// export const cloneableGenerator = generatorFunc => (...args) => {
+//     const history = []
+//     const gen = generatorFunc(...args)
+//     return {
+//         next: arg => {
+//             history.push(arg)
+//             return gen.next(arg)
+//         },
+//         clone: () => {
+//             const clonedGen = cloneableGenerator(generatorFunc)(...args)
+//             history.forEach(arg => clonedGen.next(arg))
+//             return clonedGen
+//         },
+//         return: value => gen.return(value),
+//         throw: exception => gen.throw(exception),
+//     }
+// }
+
+
+export class Reader {
+    constructor(saga, args) {
+        this.saga = saga;
+        this.gen = getIteratorOrThrow(saga)(...args);
+    }
+
+    read(iter) {
+        iter = getIteratorOrThrow(iter);
+        while(this.step(iter));
+    }
+
+    step(iter) {
+        let yielded;
+
+        obj2 = iter.next(value => {
+            yielded = value;
+        });
+
+        if(obj2.done) {
+            if(typeof obj2.value !== 'undefined') {
+                // Return was called.
+                obj1 = iterator1.next(yielded);
+                if(obj1.done !== true) {
+                    throw new Error('Expected saga to end.');
+                }
+            }
+            return false;
+        }
+
+        obj1 = this.saga.next(yielded);
+
+        if(obj1.done) {
+            throw new Error('Saga ended abruptly.');
+        }
+
+        compare(obj1.value, obj2.value);
+    }
+}
+
+
